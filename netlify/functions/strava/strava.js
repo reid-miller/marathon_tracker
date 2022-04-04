b86275451e68965ba187ab177152ac814e7bef58
Reid <48286383+reid-miller@users.noreply.github.com>
2022-04-04 14:19:04 -0500

    Update strava.js

--- netlify/functions/strava/strava.js
+++ netlify/functions/strava/strava.js
@@ -1,17 +1,53 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+import fetch from 'node-fetch';
+
+const STRAVA_CLIENT_SECRET = process.env.strava_client_secret;
+const STRAVA_CLIENT_ID = process.env.strava_client_id;
+
+export async function handler (event, context) {
+  //...
+  if (params.code) {
+          // get token!
+          let token = await getToken(params.code)
+          console.log('token=',token)
+          return {
+              statusCode : 200,
+              body : JSON.stringify({
+                  token
+              })
+          }
+      }
+  // ...
 }
 
-module.exports = { handler }
+ /* 
+ Original API Docs:
+ 
+ curl -X POST https://www.strava.com/api/v3/oauth/token \
+  -d client_id=ReplaceWithClientID \
+  -d client_secret=ReplaceWithClientSecret \
+  -d code=ReplaceWithCode \
+  -d grant_type=authorization_code
+  */
+async function getToken (code) {
+  // Translated into node fetch...
+    let response = await fetch(
+        'https://www.strava.com/api/v3/oauth/token',
+    {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(
+            {
+                client_id : STRAVA_CLIENT_ID,
+                client_secret : STRAVA_CLIENT_SECRET,
+                code : code,
+                grant_type : 'authorization_code'
+
+            }
+        )
+    }
+    );
+    let token = await response.json();
+    return token;
+}
